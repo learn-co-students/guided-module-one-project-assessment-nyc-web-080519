d65b793a4f2bfb1c49bb5050d8173ec167926b68
aresnik11 <aresnik11@gmail.com>
2019-08-20 18:05:00 -0400

    beginning stages of CLI

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,13 +2,18 @@ class CommandLineInterface
 
     def greet
         puts "Welcome to Hogwarts School of Witchcraft and Wizardry!"
+
+        puts "What is your name?"
+        name = gets.chomp
+
         puts "Are you a returning student? (Y/N)"
         returning_student = gets.chomp
 
         good_answer = false
         while good_answer == false    
             if returning_student == "Y"
-                #something
+                #check them in / look them up
+                check_in(name)
                 good_answer = true
             elsif returning_student == "N"
                 #something
@@ -18,9 +23,23 @@ class CommandLineInterface
             end
         end
 
-        puts "What is your name?"
-        input = gets.chomp
-        puts input
+
+    end
+
+    def check_in(name)
+        student = Student.find_by(name: name)
+        if student
+            puts "Welcome back, #{name}! Your bags are upstairs, continue to the Great Hall!"
+            if student.dumbledores_army
+                puts "Psst, I hear there's a DA meeting in a couple hours"
+            end
+            if student.order_of_the_phoenix
+                puts "Oh, and there's important Order business. Take this envelope."
+            end
+        else
+            puts "sorry, we can't find you."
+        end
+        
     end
 
 end
